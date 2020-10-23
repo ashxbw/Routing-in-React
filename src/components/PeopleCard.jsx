bb0f90bca713fdd82ff655507778b50aaec993a2
Ashley B. Webster <70589727+ashxbw@users.noreply.github.com>
2020-10-23 08:40:43 -0500

    Update PeopleCard.jsx

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -5,7 +5,7 @@ const PeopleCard = props => {
     return (<React.Fragment><center><div class="card border-info shadow-sm center mt-5 mb-3 w-50">
         <div class="card-body text-center">
             <h5 class="card-title">{props.people.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Age: {props.people.age}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">Age - {props.people.age}</h6>
             <p class="card-text">{props.people.gender}</p>
             <Link to={`/People/${props.people.id}`}>
                 <button class="btn btn-info">See More</button>
