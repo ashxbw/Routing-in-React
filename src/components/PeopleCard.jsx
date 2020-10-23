f93fdbbef551e3a71f7839a1e513c2c8ba638dba
Ashley B. Webster <70589727+ashxbw@users.noreply.github.com>
2020-10-23 08:38:43 -0500

    Update PeopleCard.jsx

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const PeopleCard = props => {
-    return (<React.Fragment><center><div class="card border-info center mt-5 mb-3 w-50">
+    return (<React.Fragment><center><div class="card border-info shadow-sm center mt-5 mb-3 w-50">
         <div class="card-body text-center">
             <h5 class="card-title">{props.people.name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">Age: {props.people.age}</h6>
